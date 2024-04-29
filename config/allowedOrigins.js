7f41aadc224b30f471bf51b39c08d858747eb1e5
Jan Garay <jangaray3@gmail.com>
2024-04-28 23:36:04 -0500

    proyecto

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
+    'localhost:3000',
     'https:www.GarayReparaciones.com',
     'https://GarayReparaciones.com'
 ]
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,11 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const asyncHandler = require('express-async-handler')
-
-
 
 // @desc Login
 // @route POST /auth
 // @access Public
-const login = asyncHandler(async (req, res) => {
+const login = async (req, res) => {
     const { username, password } = req.body
 
     if (!username || !password) {
@@ -32,14 +29,14 @@ const login = asyncHandler(async (req, res) => {
                 "roles": foundUser.roles
             }
         },
-        `${process.env.ACCESS_TOKEN_SECRET}`,
-        { expiresIn: '2m' }
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '15m' }
     )
 
     const refreshToken = jwt.sign(
         { "username": foundUser.username },
-        `${process.env.REFRESH_TOKEN_SECRET}`,
-        { expiresIn: '1d' }
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: '7d' }
     )
 
     // Create secure cookie with refresh token 
@@ -52,7 +49,7 @@ const login = asyncHandler(async (req, res) => {
 
     // Send accessToken containing username and roles 
     res.json({ accessToken })
-})
+}
 
 // @desc Refresh
 // @route GET /auth/refresh
@@ -66,8 +63,8 @@ const refresh = (req, res) => {
 
     jwt.verify(
         refreshToken,
-        `${process.env.REFRESH_TOKEN_SECRET}`,
-        asyncHandler(async (err, decoded) => {
+        process.env.REFRESH_TOKEN_SECRET,
+        async (err, decoded) => {
             if (err) return res.status(403).json({ message: 'Forbidden' })
 
             const foundUser = await User.findOne({ username: decoded.username }).exec()
@@ -81,12 +78,12 @@ const refresh = (req, res) => {
                         "roles": foundUser.roles
                     }
                 },
-                `${process.env.ACCESS_TOKEN_SECRET}`,
+                process.env.ACCESS_TOKEN_SECRET,
                 { expiresIn: '15m' }
             )
 
             res.json({ accessToken })
-        })
+        }
     )
 }
 
